871281acdae61c3cd38b30251c115646bec7e9de
M-Munim <muhammadmunimoff330@gmail.com>
2025-02-20 21:32:27 +0500

    bgslider added

--- client/src/components/Steps.jsx
+++ client/src/components/Steps.jsx
@@ -7,23 +7,23 @@ const Steps = () => {
             <h1 className='text-center text-2xl md:text-3xl lg:text-4xl font-bold bg-gradient-to-l from-cyan-600 to-blue-600 bg-clip-text text-transparent'>Steps to remove background <br /> image in seconds</h1>
 
             <div className='flex items-start flex-wrap gap-4 my-16 xl:mt-24 justify-center bg-red-200'>
-                <div className='flex items-start gap-4 bg-white  drop-shadow-md p-7 pb-10 rounded-lg hover:scale-105 transition-all duration-300 min-w-[350px]'>
+                <div className='flex items-start gap-4 bg-white  drop-shadow-md p-7 pb-10 rounded-lg hover:scale-105 transition-all duration-300'>
                     <img className='max-w-9' src={assets.upload_icon} alt="" />
                     <div>
                         <p className='text-xl font-medium'>Effortless Upload</p>
-                        <p className='text-sm text-neutral-500 mt-1'>Quickly upload your images to get started <br /> in just a click!</p>
+                        <p className='text-sm text-neutral-500 mt-1'>Quickly upload your images to get<br /> started  in just a click!</p>
                     </div>
                 </div>
 
-                <div className='flex items-start gap-4 bg-white  drop-shadow-md p-7 pb-10 rounded-lg hover:scale-105 transition-all duration-300 min-w-[350px]'>
+                <div className='flex items-start gap-4 bg-white  drop-shadow-md p-7 pb-10 rounded-lg hover:scale-105 transition-all duration-300 '>
                     <img className='max-w-9' src={assets.remove_bg_icon} alt="" />
                     <div>
-                        <p className='text-xl font-medium'>One-Click Background Removal</p>
+                        <p className='text-xl font-medium'>One-Click  Removal</p>
                         <p className='text-sm text-neutral-500 mt-1'>Instantly remove backgrounds for a clean, <br />polished look.</p>
                     </div>
                 </div>
 
-                <div className='flex items-start gap-4 bg-white  drop-shadow-md p-7 pb-10 rounded-lg hover:scale-105 transition-all duration-300 min-w-[350px]'>
+                <div className='flex items-start gap-4 bg-white  drop-shadow-md p-7 pb-10 rounded-lg hover:scale-105 transition-all duration-300'>
                     <img className='max-w-9' src={assets.download_icon} alt="" />
                     <div>
                         <p className='text-xl font-medium'>High-Quality Download</p>
